de5bc53a1445495c5287351648e365ba9ad967cf
Victorohrn <Victorohrn@users.noreply.github.com>
2020-06-02 14:47:37 +0200

    Some changes

--- game.py
+++ game.py
@@ -34,7 +34,7 @@ class TheGame(arcade.Window):
         self.set_mouse_visible(False)
 
         
-
+        self.Difficulty = 0
         
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
@@ -45,7 +45,7 @@ class TheGame(arcade.Window):
         #arcade.configure_logging()
         self.gun_sound = arcade.load_sound(":resources:sounds/hurt5.wav")
         self.hit_sound = arcade.load_sound(":resources:sounds/hit5.wav")
-
+        self.pause = False
     def setup_level_one(self):
         
         self.enemy_textures = []
@@ -76,13 +76,26 @@ class TheGame(arcade.Window):
                 #add later!!!
                 self.enemy_list.append(enemy)
     def shield(self, x_start):
-       
-        shield_block_width = 5
-        shield_block_height = 10
-        shield_width_count = 20
-        shield_height_count = 5
+        shield_block_width = 1
+        shield_block_height = 1
+        shield_width_count = 1
+        shield_height_count = 1
+        
+        if self.Difficulty == 1:
+            shield_block_width = 5
+            shield_block_height = 10
+            shield_width_count = 20
+            shield_height_count = 5
+
+        elif self.Difficulty == 2:
+            shield_block_width = 2
+            shield_block_height = 5
+            shield_width_count = 10
+            shield_height_count = 2
+
         
         y_start = 150
+        
         for x in range(x_start, x_start + shield_width_count * shield_block_width, shield_block_width):
             for y in range(y_start, y_start + shield_height_count * shield_block_height, shield_block_height):
                 shield_sprite = arcade.SpriteSolidColor(shield_block_width, shield_block_height, arcade.color.GREEN)
@@ -107,11 +120,11 @@ class TheGame(arcade.Window):
 
      
         self.player_sprite = arcade.Sprite("./static/shooter.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 500
+        self.player_sprite.center_x = SCREEN_WIDTH/2
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
-       
+
         for x in range(250, 1670, 190):
             self.shield(x)
 
@@ -127,20 +140,28 @@ class TheGame(arcade.Window):
         Ritar du exempelvis bakgrund sist, så är
         bakgrunden överst! Ordningen spelar roll.
       """
-        self.enemy_list.draw()
-        self.player_bullet_list.draw()
-        self.enemy_bullet_list.draw()
-        self.shield_list.draw()
-        self.player_list.draw()
+        if self.Difficulty != 0:
+            self.enemy_list.draw()
+            self.player_bullet_list.draw()
+            self.enemy_bullet_list.draw()
+            self.player_list.draw()
+            self.shield_list.draw()
 
+        if self.Difficulty == 0:
+            arcade.draw_text(f"Press 1 for Normal\n Press 2 for Hard",  SCREEN_WIDTH / 2 - 175, 500, arcade.color.WHITE, 55 , 750)
+
+        
         # Render the text
         arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
         arcade.draw_text(f" ESC TO EXIT\n R to restart\n P to pause for 10s", 1770, 20, arcade.color.WHITE, 14)
         # Draw game over if the game state is such
+        
         if self.game_state == GAME_OVER:
-            arcade.draw_text(f"GAME OVER", SCREEN_WIDTH / 2 - 75, 500, arcade.color.WHITE, 55 , 750)
+            arcade.draw_text(f"GAME OVER", SCREEN_WIDTH / 2 - 175, 500, arcade.color.WHITE, 55 , 750)
             self.set_mouse_visible(True)
-
+        if self.pause:
+            arcade.draw_text(f"PAUSED", SCREEN_WIDTH / 2 - 175, 500, arcade.color.WHITE, 55 , 750)
+            self.set_mouse_visible(True)
     def update_enemies(self):
 
         
@@ -169,6 +190,8 @@ class TheGame(arcade.Window):
                 else:
                     enemy.texture = self.enemy_textures[1]
 
+  
+
     def allow_enemies_to_fire(self):
         """
         See if any enemies will fire this frame.
@@ -187,9 +210,19 @@ class TheGame(arcade.Window):
               
                 bullet.angle = 180
 
-                
+                bullet.change_x = random.randrange(-5,5,1) 
+                """
+                if bullet.change_x <=2:
+                    bullet.angle = 150
+                if bullet.change_x == 1:
+                    bullet.angle = 170
+                if bullet.change_x >=-2:
+                    bullet.angle = 210
+                if bullet.change_x ==-1:
+                    bullet.angle = 190
+                """
                 bullet.change_y = -BULLET_SPEED
-
+                
                 
                 bullet.center_x = enemy.center_x
                 bullet.top = enemy.bottom
@@ -261,9 +294,23 @@ class TheGame(arcade.Window):
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
             exit()
+
         if key == arcade.key.P:
-            arcade.pause(10)
+            self.pause = not self.pause
+
         if key == arcade.key.R:
+            self.play()
+            self.set_mouse_visible(False)
+            
+        if key == arcade.key.KEY_1:
+            self.Difficulty = 1
+            self.play()
+            self.set_mouse_visible(False)
+            """
+            self.shield_list.shield_sprite.remove_from_sprite_lists()
+           """
+        if key == arcade.key.KEY_2:
+            self.Difficulty = 2
             self.play()
             self.set_mouse_visible(False)
     def on_mouse_motion(self, x, y, dx, dy):
@@ -314,6 +361,8 @@ class TheGame(arcade.Window):
         Alltså här skriver du in ordning för när
         alla spelobjekt ska uppdateras.
         """
+        if self.pause:
+            return
 
         self.update_enemies()
         self.allow_enemies_to_fire()
